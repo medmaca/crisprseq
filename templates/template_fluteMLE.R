375c82b043cc48d7deb21161cb6652cf500e8586
medmaca <matthew.care@gmail.com>
2024-11-01 15:59:40 +0000

    Addition to resolve Apptainer issue.

--- templates/template_fluteMLE.R
+++ templates/template_fluteMLE.R
@@ -5,12 +5,13 @@
     #### graphs mageck MLE
 
     # Required to fix corrupted cache from Singularity container
-    library(BiocFileCache)
-    bfc <- BiocFileCache("~/.cache/R/ExperimentHub")
-    res <- bfcquery(bfc, "experimenthub.index.rds", field="rname", exact=TRUE)
-    bfcremove(bfc, rids=res\$rid)
-    library(ExperimentHub)
-    eh = ExperimentHub()
+    Sys.setenv(HOME = getwd())
+    # library(BiocFileCache)
+    # bfc <- BiocFileCache("~/.cache/R/ExperimentHub")
+    # res <- bfcquery(bfc, "experimenthub.index.rds", field="rname", exact=TRUE)
+    # bfcremove(bfc, rids=res\$rid)
+    # library(ExperimentHub)
+    # eh = ExperimentHub()
 
     library(MAGeCKFlute)
     library(clusterProfiler)
